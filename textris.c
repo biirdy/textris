3b8ef7cc4623eb19c606d4df84120809911bebf9
Jamie Bird <jamie06bird@gmail.com>
2016-05-19 15:39:12 +0100

    Selection now done using buckets

--- textris.c
+++ textris.c
@@ -21,18 +21,19 @@ typedef struct{
 typedef struct{
 	int xy[20][20];
 	int xy_col[10][20];
-
-	/*
-	int x;
-	int y;
-	*/
 } board;
+
+typedef struct{
+	int bucket1[14];
+}bucket;
 
 typedef struct{
 	shape * s;
 	board * b;
-	/* data */
+	bucket * bucket;
 } thread_params;
+
+
 
 typedef int *(*fptr)(int i, int j);
 
@@ -40,12 +41,14 @@ typedef int *(*fptr)(int i, int j);
 char shape_bytes[7][4] = {
 	{0b00000000, 0b00000000, 0b00000000, 0b00001111}, 	// i
 	{0b00000000, 0b00000100, 0b00001110, 0b00000000}, 	// t
-	{0b00000000, 0b00001000, 0b00001000, 0b0001100}, 	// l
+	{0b00000000, 0b00001000, 0b00001000, 0b00001100}, 	// l
 	{0b00000000, 0b00000001, 0b00000001, 0b00000011}, 	// l - reverse
 	{0b00000000, 0b00001100, 0b00000110, 0b00000000}, 	// z
-	{0b00000000, 0b00000011, 0b00000110, 0b00000000, }, // z - reverse
-	{0b00000000, 0b00000110, 0b00000110, 0b00000000, }  // square 
+	{0b00000000, 0b00000011, 0b00000110, 0b00000000}, // z - reverse
+	{0b00000000, 0b00000110, 0b00000110, 0b00000000}  // square 
 };
+
+
 
 int DEBUG_CNT = 0;
 
@@ -86,14 +89,7 @@ fptr get_map_func(shape s, int direction){
 		return map_rotation3;
 }
 
-void new_shape(shape * s){
-	//new shape
-	s->x = 3;
-	s->y = -1;
-	s->type++;
-	s->type = rand() % 6;
-	s->rotation = 0;
-}
+
 
 void draw_shape(shape s){
 
@@ -108,7 +104,6 @@ void draw_shape(shape s){
 			map = map_func(i , j);
 
 			if(CHECK_BIT(shape_bytes[s.type][BYTE_MAP(map)], BIT_MAP(map))){
-				//mvprintw(s.y + Y_MAP(map), (s.x + X_MAP(map)) * 2 + 1, CHECK_BIT(shape_bytes[s.type][BYTE_MAP(map)], BIT_MAP(map)) ? "# " : "");
 				mvaddch(s.y + Y_MAP(map), (s.x + X_MAP(map)) * 2 + 1,  ' '|A_REVERSE);
 				mvaddch(s.y + Y_MAP(map), (s.x + X_MAP(map)) * 2 + 2,  ' '|A_REVERSE);
 			}
@@ -117,24 +112,6 @@ void draw_shape(shape s){
 	}
 	attroff(COLOR_PAIR(s.type+1));
 }
-
-//TODO: only clear set bits
-void clear_shape(shape s){
-	int byte, bit;
-
-	//bytes
-	for(byte = 0; byte < 4; byte++){
-
-		char c_byte = shape_bytes[s.type][byte];
-
-		//bit
-		for(bit = 0; bit < 4; bit++){
-			mvprintw(s.y+byte, s.x+(bit*2), c_byte & 0x01 ? "  " : "");
-			c_byte = c_byte >> 1;
-		}
-	}
-}
-
 
 void draw_board(board b){
 
@@ -146,28 +123,33 @@ void draw_board(board b){
 				mvprintw(y, (x*2)+1, "  ");
 			}else{
 				attron(COLOR_PAIR(b.xy[x][y]));
-				//mvprintw(y, (x*2)+1, "# ");
 				mvaddch(y, (x*2) + 1, ' '|A_REVERSE);
 				mvaddch(y, (x*2) + 2, ' '|A_REVERSE);
 				attroff(COLOR_PAIR(b.xy[x][y]));
 			}
 		}
 	}
-
 }
 
-int check_move(shape s, board b, int x_direction, int y_direction){
+int check_move(shape s, board b, int x_direction, int y_direction, int rotation){
+
+	shape s_new = s;
+	s_new.rotation += rotation;
+	if(s_new.rotation == 4)
+		s_new.rotation = 0;
+	else if(s_new.rotation == -1)
+		s_new.rotation = 3; 
 
 	int i, j;
 	int * map;
-	int * (*map_func)(int i, int j) = get_map_func(s, 0);
+	int * (*map_func)(int i, int j) = get_map_func(s_new, 0);
 
 	for(i = 0; i < 4; i++){
 		for(j = 0; j < 4; j++){
 
 			map = map_func(i , j);
 
-			if(CHECK_BIT(shape_bytes[s.type][BYTE_MAP(map)], BIT_MAP(map))){
+			if(CHECK_BIT(shape_bytes[s_new.type][BYTE_MAP(map)], BIT_MAP(map))){
 
 				//check board boundaries
 				if(	(s.x + X_MAP(map) + x_direction) < 0 || 
@@ -178,7 +160,7 @@ int check_move(shape s, board b, int x_direction, int y_direction){
 				}
 
 				//check within board
-				if(b.xy[s.x + X_MAP(map) + x_direction][s.y + Y_MAP(map) + y_direction] > 0){
+				if(b.xy[s.x + X_MAP(map) + x_direction][s_new.y + Y_MAP(map) + y_direction] > 0){
 					return 0;
 				}
 			}
@@ -187,8 +169,68 @@ int check_move(shape s, board b, int x_direction, int y_direction){
 	return 1;
 }
 
-int check_rotation(shape s, board b, int r_direction){
+int * random_bucket(){
+	int new_b[] = {0,1,2,3,4,5,6};
 
+	int i;
+    for (i = 0; i < 6; i++){
+		int j = i + rand() / (RAND_MAX / (7 - i) + 1);
+		int t = new_b[j];
+		new_b[j] = new_b[i];
+		new_b[i] = t;
+    }
+
+    return new_b;
+}
+
+void init_bucket(bucket * b){
+
+	int * b1 = random_bucket();
+	int * b2 = random_bucket();
+
+	int i;
+
+	//both buckets
+	for(i=0 ; i<7; i++){
+		b->bucket1[i] = *(b1 + i);
+		b->bucket1[i+7] =  *(b2 + i);
+	}
+}
+
+void refill_bucket(bucket * b){
+
+	int * b1 = random_bucket();
+	int i;
+
+	//second bucket
+	for(i=0 ; i<7; i++)
+		b->bucket1[i+7] = *(b1 + i);
+}
+
+void draw_bucket(bucket * b){
+	int i;
+	for(i =0; i<14; i++){
+		mvprintw(0 + i, 40, "%d Bucket %d type %d       ", i, i/7 + 1, b->bucket1[i]);
+	}
+}
+
+void new_shape(shape * s, bucket * b){
+	s->x = 3;
+	s->y = -1;
+	s->type = b->bucket1[0];
+	s->rotation = 0;
+
+	//check if second bucket is empty
+	if(b->bucket1[7] == -1)
+		refill_bucket(b);
+
+	//shuffle buckets
+	int i;
+	for(i = 0; i < 13; i++)
+		b->bucket1[i] = b->bucket1[i+1];
+	b->bucket1[13] = -1;
+
+	draw_bucket(b);
 }
 
 int add_to_board(shape s, board * b){
@@ -228,12 +270,28 @@ void drop_loop(void * tp_void_ptr){
 
 	while(1){
 
-		if(check_move(*tp_ptr->s, *tp_ptr->b, 0, 1)){
+		if(check_move(*tp_ptr->s, *tp_ptr->b, 0, 1, 0)){
 			tp_ptr->s->y++; 
+		}else{
+
+			if(add_to_board(*tp_ptr->s, tp_ptr->b) <= 0){
+				mvprintw(40 + DEBUG_CNT++, 0, "GAME OVER");
+				break;
+			}
+
+			
+
+			int clears = check_lines(tp_ptr->b);
+			if(clears > 0){
+				mvprintw(40 + DEBUG_CNT++, 0, "Cleared lines %d", clears);
+			}
+
+			new_shape(tp_ptr->s, tp_ptr->bucket);
+			
+
 		}
 
-		//100ms
-		usleep(250000);
+		usleep(250000);		//100ms
 	}
 }
 
@@ -254,9 +312,7 @@ int check_lines(board * b){
 	int x, y, blk_cnt, row_cnt = 0;
 
 	for(y = 0; y < 20; y++){
-
 		blk_cnt = 0;
-
 		for(x = 0; x < 10; x++){
 			if(b->xy[x][y] > 0)
 				blk_cnt++;
@@ -266,18 +322,12 @@ int check_lines(board * b){
 			clear_line(b, y);
 		}
 	}
-
 	return row_cnt;
 }
 
-
-
-//TODO add dropping to a thread - constant speed without keys
-int main(){
-
+int main(){  
 
 	srand(time(NULL));
-	int r =  
 
 	setlocale(LC_ALL, "");
 
@@ -307,17 +357,18 @@ int main(){
 		mvprintw(20,i,"_");	
 	}
 
+	bucket bu;
+	init_bucket(&bu);
+
 	shape s;
-	s.x = 1;
-	s.y = 0;
-	s.rotation = 0;
-	s.type = 6;
+	new_shape(&s, &bu);
 
 	board b = init_board();
 
 	thread_params tp;
 	tp.b = &b;
 	tp.s = &s;
+	tp.bucket = &bu;
 
 	pthread_t pth;
     pthread_create(&pth, NULL, drop_loop, (void * ) &tp);
@@ -326,24 +377,6 @@ int main(){
 	while(1){
 
 		int ch = getch();
-
-		if(check_move(s, b, 0, 1)){
-			//s.y++;
-		}else{
-
-			if(add_to_board(s, &b) <= 0){
-				mvprintw(40 + DEBUG_CNT++, 0, "GAME OVER");
-				break;
-			}
-
-			new_shape(&s);
-
-			int clears = check_lines(&b);
-			if(clears > 0){
-				mvprintw(0, 40, "Cleared lines %d", clears);
-			}
-
-		}
 
 		draw_board(b);
 		
@@ -356,15 +389,15 @@ int main(){
 					s.type = 0;
 
 			//left
-			}else if(ch == KEY_LEFT && check_move(s, b, -1, 0)){
+			}else if(ch == KEY_LEFT && check_move(s, b, -1, 0, 0)){
 				s.x--;
 
 			//right
-			}else if(ch == KEY_RIGHT && check_move(s, b, 1, 0)){
+			}else if(ch == KEY_RIGHT && check_move(s, b, 1, 0, 0)){
 				s.x++;
 
 			//rotate
-			}else if(ch == KEY_DOWN /* && check_rotation()*/){
+			}else if(ch == KEY_DOWN && check_move(s, b, 0, 0, 1)){
 				s.rotation++;
 				if(s.rotation == 4)
 					s.rotation = 0;
@@ -372,7 +405,7 @@ int main(){
 			//hard drop
 			}else if(ch == ' '){
 				while(true){
-					if(!check_move(s, b, 0, 1))
+					if(!check_move(s, b, 0, 1, 0))
 						break;
 					s.y++;
 				}
@@ -381,7 +414,6 @@ int main(){
 
 		draw_shape(s);
 
-		//show positions
 		mvprintw(30, 2, "X %d\n", s.x);
 		mvprintw(31, 2, "Y %d\n", s.y);
 
